c03756aee0e39f36c67d5f53c17a59ba0623db25
takahara tomoya <t.tkorock221@gmail.com>
2023-10-08 23:43:05 +0900

    誤ったファイルを消した

--- test/src/hooks/createSanityPath.ts
+++
@@ -1,7 +0,0 @@
-import { SANITY_PROJECT_ID, SANITY_DATASET } from '$env/static/private';
-
-const domain = https://cdn.sanity.io/images/
-
-export const createImaePath = (asset) => {
-    const url = domain + SANITY_PROJECT_ID + '/' 'SANITY_DATASET'
-}
---
+++ test/src/hooks/fetchContent.ts
@@ -0,0 +1,33 @@
+import { SANITY_PROJECT_ID, SANITY_DATASET } from '$env/static/private';
+import {createClient} from "@sanity/client";
+
+const client = createClient({
+    projectId: SANITY_PROJECT_ID,
+    dataset: SANITY_DATASET,
+    apiVersion: "2023-10-05",
+    useCdn: false
+})
+
+export const fetchContents = async() => {
+    try {
+        const data = await client.fetch(`*[_type == "content"]`);
+        return data;
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+export const fetchContentFromSlug = async(slug:string) => {
+    try {
+        const data = await client.fetch(`*[_type == "content" && slug.current == "${slug}"]{
+            title,
+            tags,
+            slug,
+            published,
+            "imageUrl" :mainVisual.asset->url
+        }[0]`);
+        return data;
+    } catch(e) {
+        console.log(e)
+    }
+}
