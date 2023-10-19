8b77fdc5c2ab46beea4809cf0e4a82b90f1eeabd
takahara tomoya <t.tkorock221@gmail.com>
2023-10-19 09:22:59 +0900

    カードを作った

--- test/src/routes/tags/[slug]/+page.svelte
+++ test/src/routes/tags/[slug]/+page.svelte
@@ -1,14 +1,34 @@
-<script>
-	import Card from '@smui/card';
+<script lang="ts">
+	import Card, {PrimaryAction, Content, Media, MediaContent} from '@smui/card';
 	export let data;
 	const {filterContents} = data;
+
+    const pageMove = (slug:string) => {
+        window.location.href = `/contents/${slug}`
+    }
 </script>
 
-
+<div class="cardBox">
 	{#each filterContents as content}
-        <Card>
-            <a href="/contents/{content.slug.current}">
+        <Card style="width: 300px;">
+            <PrimaryAction on:click={() => pageMove(content.slug.current)} padded>
+                <Media aspectRatio="square">
+                    <MediaContent>
+                        <img src={content.imageUrl} style:width="100%" alt="">
+                    </MediaContent>
+                </Media>
                 {content.title}
-            </a>
+            </PrimaryAction>
 		</Card>
     {/each}
+</div>
+
+<style>
+    .cardBox {
+        margin: 0 auto;
+        padding: 24px 0;
+        width: 840px;
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
